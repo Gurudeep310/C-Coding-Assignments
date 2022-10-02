42c48511f3c9ec669d787f1de73f037a29228e14
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 18:59:23 +0530

    Read a 2D-array

---
+++ Assignments/1.5 Lab 9/Prog_1_5_9.c
@@ -0,0 +1,52 @@
+/* [L9 P9] Read a 2D-array
+Write a function to read values of a number of rows, number of columns, 2 dimensional (2D) array elements and display the 2D array in a matrix form.
+Read this article to understand how 2-d arrays work: https://www.programiz.com/c-programming/c-multi-dimensional-arrays
+Input 
+    2 
+    3 
+    1 4 5 
+    2 3 0 
+    Where, 
+First line of represents the number of rows.  
+Second line of input represents the number of columns.  
+Third line contains array elements of the 1st row and so on. 
+Output 
+    1 4 5 
+    2 3 0
+    where
+There must be single space between 2 numbers in the row.
+There should not be any space after the last number in the row. e.g. in the 2nd row, there should not be any space after number 0. 
+Assume that, 
+Row and column values are integers within the range [1 to 100]. 
+2D Array elements are within the range [-2147483648 to 2147483647].*/
+
+#include<stdio.h>
+int main()
+{
+    int m,n;
+    scanf("%d",&m);
+    scanf("%d",&n);
+    int arr[m][n];
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(j==n-1)
+            {
+                printf("%d",arr[i][j]);
+            }
+            else
+            {
+                printf("%d",arr[i][j]);
+            }
+        }
+    }
+    return 0;
+}
