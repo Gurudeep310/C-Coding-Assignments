22c15e6862ddb1bc0850c032d7805d172cf142f3
Gurudeep <gurudheep2002@gmail.com>
2022-10-02 19:07:59 +0530

    Find the "Kth" max and min element of an array

---
+++ Data Structures/3_kth_max_and_min.c
@@ -0,0 +1,48 @@
+/*
+Find the "Kth" max and min element of an array
+*/
+
+#include <stdio.h>
+
+void bubbleSort(int* arr, int size)
+{
+    int temp=0;
+    while(1 == 1)
+    {
+        int flag=0;
+        for(int i=0;i<size-1;i++)//Go through all the elements
+        {
+            if(arr[i]>arr[i+1])
+            {
+                temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+                flag=flag+1;
+            }
+        }    
+        //If you find any consecutive elements such that arr[i] > arr[i+1] swap them.
+        //If you don't find any swap-worthy pairs at all: you are done, break from the while loop
+        if(flag==0)
+        {
+            break;
+        }
+    }
+}
+
+int main(){
+    int arr[20000]; //the arrays itself has 20000 elements. But we only use N slots.
+    int N;
+    scanf("%d", &N); //takes the size of the arrary from the user.
+    for(int i = 0; i < N; i++){
+        scanf("%d", &arr[i]);   //scans the elements of the array
+    }   
+    bubbleSort(arr, N); //Notice that we are passing the array name: which is in fact the pointer to the first element of the array.    
+    for(int i = 0;i<N;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    int k;
+    scanf("%d",&k);
+    printf("%dth minimum element is %d and %dth maximum element is %d",k,arr[k-1],k,arr[N-k]);
+    return 0;
+}
